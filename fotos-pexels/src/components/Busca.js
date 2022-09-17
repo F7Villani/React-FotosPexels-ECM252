87a186fc35b230406caea4be8a3cb99a7dab5b36
Felipe Villani <felipevillani200@hotmail.com>
2022-09-17 12:57:56 -0300

    feat: iniciando Busca

---
+++ fotos-pexels/src/components/Busca.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+
+export class Busca extends Component {
+
+    state = {
+        termoDeBusca: ''
+    }
+
+    render() {
+        return (
+        
+            <div className="flex flex-column">
+                <span className="p-input-icon-left w-full">
+                    <i className="pi pi-search"></i>
+                    <InputText className='w-full'/>
+                </span>
+                <Button 
+                    label = 'OK'
+                    className='p-button-outlined mt-2'
+                />
+            </div>
+
+        )
+    }
+}
+
+export default Busca
